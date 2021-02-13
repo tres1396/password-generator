b14153b21635027adfa329a54ed41ad98550d8f0
Tre Scott <tres1396@gmail.com>
2021-02-13 18:37:02 -0500

    Edited relative file paths

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,20 @@ var passLowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o'
     passNumbers = [1,2,3,4,5,6,7,8,9,0];
     passSpecial = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','['];
 
-//password code
+
+//global variable
+var newPassword = []
+
+
+
+//User password prompts
 function passwordOptions(){
   var length = parseInt(prompt("How many characters would you like your password to be? (Must be between 8 and 128 characters!"))
 
-  if (isNAN(length)===true) {
-    alert("Please choose a number between 8 and 128!)
+  if (isNAN(length)===true){
+    alert("Please choose a number between 8 and 128!")
   }
-
+  
   if (length < 8) {
     alert("Password must be at least 8 characters!")
   }
@@ -30,4 +36,49 @@ if (lowercaseChars===false && uppercaseChars===false && numericChars===false && 
   return
 }
 
+if (lowercaseChars === true) {
+  (newPassword.push(passLowercase));
 }
+
+if (uppercaseChars === true) {
+  (newPassword.push(passUppercase));
+}
+
+if (numericChars === true) {
+  (newPassword.push(passNumbers));
+}
+
+if (specialChars === true) {
+  (newPassword.push(passSpecial));
+}
+
+var passwordChoices = {
+  length: length,
+  lowercaseChars: lowercaseChars,
+  uppercaseChars: uppercaseChars,
+  numericChars: numericChars,
+  specialChars: specialChars,
+}
+return passwordChoices
+}
+
+//Generate password using above criteria
+function generatePassword(){
+  var newPWstr = newPassWd.join("");
+ 
+  var password = "";
+  
+  for (var i = 0; i < length; i++) {
+  var genChr = newPWstr.charAt(Math.floor(Math.random() * newPWstr.length));
+  password = password.concat(genChr);
+  }
+}
+  //Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+  }
+
+  generateBtn.addEventListener("click", writePassword);
