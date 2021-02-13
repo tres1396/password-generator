2bd8832467e41431b7cdb60e8b19a37a5f556db8
Tre Scott <tres1396@gmail.com>
2021-02-13 17:12:25 -0500

    Added code to prompt user to select at least 1 character type

--- script.js
+++ script.js
@@ -1,103 +1,33 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 //Array of options for password criteria
 var passLowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     passUppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     passNumbers = [1,2,3,4,5,6,7,8,9,0];
     passSpecial = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','['];
 
+//password code
+function passwordOptions(){
+  var length = parseInt(prompt("How many characters would you like your password to be? (Must be between 8 and 128 characters!"))
 
-function generateOptions() {
-
-  var length = parseInt(prompt("How many characters would you like the password to be?"))
-
-//no password less than 8 characters
-if (length < 8) {
-  alert('The password must be at least 8 characters!');
-  return;
-}
-
-//no password more than 128 characters
-if (length > 128) {
-  alert('The password must be less than 128 characters!');
-  return;
-}
-
-for (var i = userInput; userInput < 8 || userInput > 128; userInput = prompt('How many characters would you like your password to contain?')) {
-  alert('Password must be at least 8 characters but less than 128 characters.');
-
-var includeLowercase = confirm("Do you want to include lowercase characters?");
-var includeUppercase = confirm("Do you want to include uppercase characters?");
-var includeNumbers = confirm("Do you want to include numbers?");
-var includeSpecialChars = confirm("Do you want to include special characters?")
-
-if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecialChars) {
-  alert("Your password must contain at least one special, numeric, lowercase, or uppercase character");
-  return;
-}
-
-var questionOptions = {
-length: length,
-lowerCase: includeLowercase,
-numbers: includeNumbers,
-specialCharacters: includeSpecialChars
-}
-
-return questionOptions;
-
-}
-
-
-// var passLength = window.prompt("How many characters would you like the password to be?");
-//   if (passLength)
-
-//     passLowercase = window.prompt("Would you like to include lowercase letters?");
-//     passUppercase = window.prompt("Would you like to include uppercase letters?");
-//     passNumbers = window.prompt("Would you like to include numbers?");
-//     passSpecial = window.prompt("Would you like to include special characters?");
-
-
-// Write password to the #password input
-function writePassword() {
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-
-  var options = generateOptions();
-  console.log(options)
-
-  var passwordPool = [];
-  console.log(passwordPool)
-
-  if (options.lowerCase) {
-    for (i = 0; i < passLowercase; ++i) {
-    passwordPool.push(onlyLower[i]);
-    }
-}
-if (options.upperCase) {
-  for (i = 0; i < passUppercase.length; ++i) {
-  passwordPool.push(onlyUpper[i]);
+  if (isNAN(length)===true) {
+    alert("Please choose a number between 8 and 128!)
   }
-}
-  if (options.numeric) {
-      for (i = 0; i < passNumbers.length; ++i) {
-      passwordPool.push(onlyNumeric[i]);
-      }
+
+  if (length < 8) {
+    alert("Password must be at least 8 characters!")
   }
-  if (options.specialCharacters) {
-    for (i = 0; i < passSpecial.length; ++i) {
-        passwordPool.push(onlySpecial[i]);
-    }
-} 
+  
+  if (length > 128) {
+    alert("Password must be less than 128 characters!")
+  }
 
-var finalPassword = [];
+  var lowercaseChars= confirm("Would you like to include lowercase characters?")
+  var uppercaseChars= confirm("Would you like to include uppercare characters?")
+  var numericChars= confirm("Would you like to include numeric characters?")
+  var specialChars= confirm("Would you like to include special characters?")
 
-for (let i = 0; i < options.length; ++i) {
-    var randomPicker = Math.floor(Math.random() * Math.floor(passwordPool.length));
-     finalPassword.push(passwordPool[randomPicker])
+if (lowercaseChars===false && uppercaseChars===false && numericChars===false && specialChars===false){
+  alert("You must select at least one of the character types in order to create a password!")
+  return
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+}
